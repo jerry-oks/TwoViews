f5182c9f5bd7da05399659195d1a7a0d562167af
Vadim Vanatov <79990006611@yandex.ru>
2023-07-17 22:53:59 +0300

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by HOLY NADRUGANTIX on 17.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
